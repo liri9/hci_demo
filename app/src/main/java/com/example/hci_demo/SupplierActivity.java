5ef9475cb50d9a8e538af5d3dafcde82e9e933ec
Oded <odedginat@gmail.com>
2023-04-12 14:18:03 +0300

    Added supplier activity

---
+++ app/src/main/java/com/example/hci_demo/SupplierActivity.java
@@ -0,0 +1,14 @@
+package com.example.hci_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupplierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplier);
+    }
+}
