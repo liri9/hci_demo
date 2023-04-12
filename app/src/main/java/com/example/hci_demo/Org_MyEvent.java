0f13b1d04e4ccf4ad52e008a5fd8635aab4eb4a8
liri9 <lichow.liri@gmail.com>
2023-04-12 12:35:01 +0300

    finished login

---
+++ app/src/main/java/com/example/hci_demo/Org_MyEvent.java
@@ -0,0 +1,14 @@
+package com.example.hci_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Org_MyEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_my_event);
+    }
+}
