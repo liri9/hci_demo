8e20eef7d8c87bf175dd1ddac7abe5320de4568b
Oded <oded@anzu.io>
2023-05-29 21:32:16 +0300

    Removed fonts

---
+++ app/src/main/java/com/example/hci_demo/utils/LoginDB.java
@@ -0,0 +1,17 @@
+package com.example.hci_demo.utils;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+
+public class LoginDB {
+//    private Dictionary<> vault = new ArrayList<>();
+//
+//    private void hardCoded()
+//    {
+//        vault.add(new ArrayList<>())
+//    }
+//    private ArrayList<String> createLogin(String name, String password)
+//    {
+//        return new ArrayList<>()
+//    }
+}
