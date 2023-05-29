cb932c67d91612eec8f72bf400f9381cbfb78066
Oded <oded@anzu.io>
2023-05-29 20:21:43 +0300

    Changed font names

--- app/src/main/java/com/example/hci_demo/MainActivity.java
+++ app/src/main/java/com/example/hci_demo/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.hci_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginBtn;
+    Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoActivity(LoginActivity.class);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoActivity(SignUpActivity.class);
+            }
+        });
+    }
+
+    private void gotoActivity(Class cls){
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
     }
 }
