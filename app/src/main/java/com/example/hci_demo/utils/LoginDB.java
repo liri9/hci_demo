06a878bec24f47920160fcc8a338fd40f110ba68
Oded <odedginat@gmail.com>
2023-05-31 02:33:51 +0300

    Added transitions for all account types

--- app/src/main/java/com/example/hci_demo/utils/LoginDB.java
+++ app/src/main/java/com/example/hci_demo/utils/LoginDB.java
@@ -4,14 +4,40 @@ import java.util.ArrayList;
 import java.util.Dictionary;
 
 public class LoginDB {
-//    private Dictionary<> vault = new ArrayList<>();
-//
-//    private void hardCoded()
-//    {
-//        vault.add(new ArrayList<>())
-//    }
-//    private ArrayList<String> createLogin(String name, String password)
-//    {
-//        return new ArrayList<>()
-//    }
+    private ArrayList<User> vault = new ArrayList<>();
+
+    public LoginDB() {
+    }
+
+    public void hardCoded()
+    {
+        vault.add(new User("avi@food.com", "1111", UserType.supplier));
+        vault.add(new User("ravi@food.com", "1111", UserType.supplier));
+        vault.add(new User("flop@gmail.com", "1111", UserType.guest));
+        vault.add(new User("natanel@hotmail.com", "1111", UserType.guest));
+        vault.add(new User("frigid@ymail.com", "1111", UserType.guest));
+        vault.add(new User("dana@ymail.com", "1111", UserType.orginazer));
+        vault.add(new User("worker1@rmail.com", "1111", UserType.staff));
+        vault.add(new User("worker2@rmail.com", "1111", UserType.staff));
+        vault.add(new User("worker3@rmail.com", "1111", UserType.staff));
+        vault.add(new User("staff", "1111", UserType.staff));
+        vault.add(new User("supplier", "1111", UserType.supplier));
+        vault.add(new User("guest", "1111", UserType.guest));
+        vault.add(new User("orginazer", "1111", UserType.orginazer));
+    }
+    public void add(String email, String password, UserType userType)
+    {
+        vault.add(new User(email, password, userType));
+    }
+    public User get(String email, String password)
+    {
+        for (User user :
+                vault) {
+            if (email.equals(user.getEmail()) &&
+            password.equals(user.getPassword())){
+                return user;
+            }
+        }
+        return null;
+    }
 }
