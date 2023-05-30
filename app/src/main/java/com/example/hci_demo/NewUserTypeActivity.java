d85c1d65a6deaa97d687c78c6cb0e960707a5604
liri9 <lichow.liri@gmail.com>
2023-05-30 18:56:08 +0300

    added screens

---
+++ app/src/main/java/com/example/hci_demo/NewUserTypeActivity.java
@@ -0,0 +1,14 @@
+package com.example.hci_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewUserTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_type);
+    }
+}
