94a449726e3886c225520dcedc71fe8524f2e51c
liri9 <lichow.liri@gmail.com>
2023-06-03 15:23:29 +0300

    past events works for supplier

--- app/src/main/java/com/example/hci_demo/PastEventsActivity.java
+++ app/src/main/java/com/example/hci_demo/PastEventsActivity.java
@@ -11,15 +11,16 @@ import com.example.hci_demo.utils.Event;
 import com.example.hci_demo.utils.listsDB;
 import com.google.android.material.button.MaterialButton;
 
+import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class PastEventsActivity extends AppCompatActivity {
  private RecyclerView tasks_LST_tasks;
  private MaterialButton org_myEvent_BTN_back;
  private ArrayList<Event> eventList = new ArrayList<>();
-    private Adapter_Past_Event adapter;
+
+    private Adapter_Past_Event adapter =new Adapter_Past_Event(eventList);;
 
     listsDB db = new listsDB();
     @Override
@@ -35,21 +36,21 @@ public class PastEventsActivity extends AppCompatActivity {
     }
 
     private void createPastEventList() {
-        eventList.add(new Event(new Date(2023, 3, 2), "Lior and Liora's Wedding", "A beautiful celebration of love and unity.", 5.0));
-        eventList.add(new Event(new Date(2023, 4, 13), "Amir's Bar Mitzvah", "A meaningful coming-of-age ceremony.", 4.6));
-        eventList.add(new Event(new Date(2022, 5, 25), "Yuval's Brit", "A joyous occasion celebrating a baby boy's birth.", 3.8));
-        eventList.add(new Event(new Date(2022, 2, 31), "Michael and Micha's Wedding", "A grand wedding celebration filled with love and happiness.", 5.0));
-        eventList.add(new Event(new Date(2023, 11, 15), "Ron and Rona's Wedding", "A memorable wedding ceremony and reception.", 2.4));
-        eventList.add(new Event(new Date(2023, 8, 4), "Shira's Bat Mitzvah", "A significant milestone celebration for a young girl.", 4.7));
-        eventList.add(new Event(new Date(2022, 9, 22), "Einat's Birthday Party", "A fun-filled birthday party with friends and family.", 5.0));
-        eventList.add(new Event(new Date(2023, 4, 4), "Shmuel's Bar Mitzvah", "A meaningful religious ceremony for a young boy.", 4.9));
-        eventList.add(new Event(new Date(2022, 11, 12), "Sarah and David's Wedding", "A romantic wedding celebration filled with joy.", 4.2));
-        eventList.add(new Event(new Date(2023, 1, 28), "Company Anniversary Party", "A special event marking the company's successful years.", 4.8));
-        eventList.add(new Event(new Date(2022, 7, 6), "Summer Music Festival", "An unforgettable music festival under the summer sun.", 4.5));
-        eventList.add(new Event(new Date(2023, 2, 10), "Graduation Ceremony", "A proud moment for graduates celebrating their achievements.", 4.7));
-        eventList.add(new Event(new Date(2022, 6, 18), "Family Reunion", "A joyful gathering of extended family members.", 4.3));
-        eventList.add(new Event(new Date(2023, 10, 1), "Charity Gala Dinner", "A glamorous fundraising event supporting a worthy cause.", 4.9));
-        eventList.add(new Event(new Date(2022, 3, 30), "New Year's Eve Party", "A lively celebration to welcome the new year.", 4.6));
+        eventList.add(new Event(new Date(2023-1900, 3, 2), "Lior and Liora's Wedding", "A beautiful celebration of love and unity.", 5.0));
+        eventList.add(new Event(new Date(2023-1900, 4, 13), "Amir's Bar Mitzvah", "A meaningful coming-of-age ceremony.", 4.6));
+        eventList.add(new Event(new Date(2022-1900, 5, 25), "Yuval's Brit", "A joyous occasion celebrating a baby boy's birth.", 3.8));
+        eventList.add(new Event(new Date(2022-1900, 2, 31), "Michael and Micha's Wedding", "A grand wedding celebration filled with love and happiness.", 5.0));
+        eventList.add(new Event(new Date(2023-1900, 11, 15), "Ron and Rona's Wedding", "A memorable wedding ceremony and reception.", 2.4));
+        eventList.add(new Event(new Date(2023-1900, 8, 4), "Shira's Bat Mitzvah", "A significant milestone celebration for a young girl.", 4.7));
+        eventList.add(new Event(new Date(2022-1900, 9, 22), "Einat's Birthday Party", "A fun-filled birthday party with friends and family.", 5.0));
+        eventList.add(new Event(new Date(2023-1900, 4, 4), "Shmuel's Bar Mitzvah", "A meaningful religious ceremony for a young boy.", 4.9));
+        eventList.add(new Event(new Date(2022-1900, 11, 12), "Sarah and David's Wedding", "A romantic wedding celebration filled with joy.", 4.2));
+        eventList.add(new Event(new Date(2023-1900, 1, 28), "Company Anniversary Party", "A special event marking the company's successful years.", 4.8));
+        eventList.add(new Event(new Date(2022-1900, 7, 6), "Summer Music Festival", "An unforgettable music festival under the summer sun.", 4.5));
+        eventList.add(new Event(new Date(2023-1900, 2, 10), "Graduation Ceremony", "A proud moment for graduates celebrating their achievements.", 4.7));
+        eventList.add(new Event(new Date(2022-1900, 6, 18), "Family Reunion", "A joyful gathering of extended family members.", 4.3));
+        eventList.add(new Event(new Date(2023-1900, 10, 1), "Charity Gala Dinner", "A glamorous fundraising event supporting a worthy cause.", 4.9));
+        eventList.add(new Event(new Date(2022-1900, 3, 30), "New Year's Eve Party", "A lively celebration to welcome the new year.", 4.6));
 
     }
 
@@ -71,10 +72,9 @@ public class PastEventsActivity extends AppCompatActivity {
 //        tasks_LST_tasks.setAdapter(adapter);
 //    }
     private void updateList(ArrayList<Event> events) {
-        Adapter_Past_Event.updateList(events);
+        adapter.updateList(events);
     }
     private void initList() {
-        adapter = new Adapter_Past_Event(eventList);
         // one item in a row
         tasks_LST_tasks.setLayoutManager(new LinearLayoutManager(this));
         tasks_LST_tasks.setHasFixedSize(true);
---
+++ app/src/main/java/com/example/hci_demo/utils/Adapter_Next_Event.java
@@ -0,0 +1,80 @@
+package com.example.hci_demo.utils;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatRatingBar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hci_demo.R;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
+
+public class Adapter_Next_Event extends RecyclerView.Adapter<Adapter_Next_Event.EventViewHolder> {
+
+private AppManager appManager;
+    private  List<Event> events;
+
+    public Adapter_Next_Event(List<Event> eventList) {
+        events = eventList;
+    }
+
+    public void updateList(List<Event> eventList) {
+        events = eventList;
+        //notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_past_events, viewGroup, false);
+        return new EventViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        Event event = getItem(position);
+        holder.event_LBL_name.setText(event.getName());
+        holder.event_LBL_date.setText(event.getDate().toString());
+        holder.event_LBL_description.setText(event.getDescription());
+
+        holder.event_RTG_rating.setRating((float) event.getRating());
+    }
+
+    @Override
+    public int getItemCount() {
+        return events == null ? 0 : events.size();
+    }
+
+    private Event getItem(int position) {
+        return events.get(position);
+    }
+
+
+    public class EventViewHolder extends RecyclerView.ViewHolder {
+        private MaterialTextView event_LBL_description;
+        private MaterialTextView event_LBL_name;
+        private MaterialTextView event_LBL_date;
+        private AppCompatRatingBar event_RTG_rating;
+
+        EventViewHolder(View itemView) {
+            super(itemView);
+            event_LBL_description = itemView.findViewById(R.id.event_LBL_description);
+            event_LBL_name = itemView.findViewById(R.id.event_LBL_name);
+            event_LBL_date = itemView.findViewById(R.id.event_LBL_date);
+            event_RTG_rating = itemView.findViewById(R.id.event_RTG_rating);
+
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    onGameClickListener.onClick(v, getItem(getAdapterPosition()), getAdapterPosition());
+//                }
+//            });
+        }
+    }
+
+}
